7a4354db3850badcd3ee2ec15acc23ec365dc69f
OBZORMASTER <20606070nr@gmail.com>
2025-09-30 15:59:46 +0300

    Додано поле для емейлу

--- form.php
+++ form.php
@@ -11,9 +11,11 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Безпечно отримуємо дані з форми
         $name = htmlspecialchars($_POST['user_name']);
+        $email = htmlspecialchars($_POST['user_email']);
 
         // Виводимо їх на сторінку
         echo "<p><strong>Ім'я:</strong> " . $name . "</p>";
+        echo "<p><strong>Email:</strong> " . $email . "</p>";
     } else {
         // Якщо хтось зайшов на form.php без відправки форми
         echo "<p>Форма не була відправлена.</p>";
